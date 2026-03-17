090f41cae41b781e890620ea59e27469e3e813fd
Josie5734 <armyjoe999@gmail.com>
2026-03-17 16:24:04 +0000

    window table separated for mini and main windows, mini window functions changed to use it

--- lua/neostats/window.lua
+++ lua/neostats/window.lua
@@ -2,50 +2,65 @@
 
 local M = {} --module
 
-M.window = { buf = nil, win = nil, width = 24, height = 6, padding = 1 } --table for the floating window data
+M.window = { --table for both windows
+	main = { buf = nil, win = nil, width = 10, height = 10, padding = 1 }, --main window
+	mini = { buf = nil, win = nil, width = 24, height = 6, padding = 1 }, --mini window
+}
 
---creating window with given text (table of lines)
+--creating window with generated text (table of lines)
 function M.mini_window_open(xp)
 	local buf = vim.api.nvim_create_buf(false, true) --buffer
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, M.mini_window_gen_text(xp, M.window.width, M.window.padding)) --set initial window text
+	vim.api.nvim_buf_set_lines(
+		buf,
+		0,
+		-1,
+		false,
+		M.mini_window_gen_text(xp, M.window.mini.width, M.window.mini.padding)
+	) --set initial window text
 
 	local win = vim.api.nvim_open_win(buf, false, {
 		relative = "editor",
 		anchor = "NW", --NW so that top left of window is placed at row,col
-		width = M.window.width,
-		height = M.window.height,
-		row = (vim.o.lines - 4) - M.window.height, --put window on bottom row + X for command line and lualine (and border if applicable)
-		col = vim.o.columns - M.window.width, --all the way to the right column, accounting for window width
+		width = M.window.mini.width,
+		height = M.window.mini.height,
+		row = (vim.o.lines - 4) - M.window.mini.height, --put window on bottom row + X for command line and lualine (and border if applicable)
+		col = vim.o.columns - M.window.mini.width, --all the way to the right column, accounting for window width
 		style = "minimal",
 		border = "single",
 		focusable = false, --cant click into the window
 	})
 
-	M.window.buf = buf --store references to buffer and window
-	M.window.win = win
+	M.window.mini.buf = buf --store references to buffer and window
+	M.window.mini.win = win
 end
 
 --update the mini window
 function M.mini_window_update(xp)
-	vim.api.nvim_buf_set_lines(M.window.buf, 0, -1, false, M.mini_window_gen_text(xp, M.window.width, M.window.padding)) --set updated window text
+	vim.api.nvim_buf_set_lines(
+		M.window.mini.buf,
+		0,
+		-1,
+		false,
+		M.mini_window_gen_text(xp, M.window.mini.width, M.window.mini.padding)
+	) --set updated window text
 end
 
 --close window
 function M.mini_window_close()
-	if M.window.win and vim.api.nvim_win_is_valid(M.window.win) then --if window exists
-		vim.api.nvim_win_close(M.window.win, true) --close window
-		M.window.win = nil --reset win var
+	if M.window.mini.win and vim.api.nvim_win_is_valid(M.window.mini.win) then --if window.mini exists
+		vim.api.nvim_win_close(M.window.mini.win, true) --close window.mini
+		M.window.mini.win = nil --reset win var
 	end
-	if M.window.buf and vim.api.nvim_buf_is_valid(M.window.buf) then --if buffer exists
-		vim.api.nvim_buf_delete(M.window.buf, { force = true }) --delete buffer
-		M.window.buf = nil --reset buf var
+	if M.window.mini.buf and vim.api.nvim_buf_is_valid(M.window.mini.buf) then --if buffer exists
+		vim.api.nvim_buf_delete(M.window.mini.buf, { force = true }) --delete buffer
+		M.window.mini.buf = nil --reset buf var
 	end
 end
 
 --check if mini window exists
 function M.mini_window_exists()
-	if M.window.win and M.window.buf then --if exists
-		if vim.api.nvim_win_is_valid(M.window.win) and vim.api.nvim_buf_is_valid(M.window.buf) then
+	if M.window.mini.win and M.window.mini.buf then --if exists
+		if vim.api.nvim_win_is_valid(M.window.mini.win) and vim.api.nvim_buf_is_valid(M.window.mini.buf) then
 			return true --send true if valid
 		end
 	else
